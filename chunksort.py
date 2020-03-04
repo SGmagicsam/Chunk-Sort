75c6dbd727c1c75258b5831b610c5e89a383bd62
SGmagicsam <38333794+SGmagicsam@users.noreply.github.com>
2020-03-04 15:43:16 -0700

    Chunksort took: 6.8211196

--- chunksort.py
+++ chunksort.py
@@ -1,36 +1,59 @@
 from timeit import default_timer as timer
+import numpy as np
 
 def findIndex(target, arr):
     '''
     Implementation of a binary search
     '''
+    #pos = middle index
     pos = len(arr)//2
-    
+    if not arr:
+        return 0
+    #base case
+    if len(arr) == 1:
+        if arr[0] < target:
+            return 1
+        else:
+            return 0
     if arr[pos] == target:
         return pos
-    elif(arr[pos] > arr[pos]):
+    #if the middle element is greater than our target 
+    elif(arr[pos] > target):
         return findIndex(target, arr[:pos])
+    #otherwise
     else:
-        return findIndex(target, arr[:pos])
+        return findIndex(target, arr[pos:]) + pos
     
 
 class Chunk(object):
     
     def __init__(self, arr):
-        #we store the ends of the list
+        #data stores the max and min of each chunk
         self._data = [arr[0], arr[-1]]
         self._numbers = arr
         
     def insert(self, num):
+        '''
+        Function for adding a new number to the chunk
+        basicly if a number is less than data[0] then this number is our new minimum and we toss it in 
+        if a number is greater than data[1] then that number is our new maximum, same idea
+        '''
         if num >= self._data[1]:
             self._numbers.append(num)
+            self.updateData()
         elif(num <= self._data[0]):
             self._numbers.insert(0, num)
+            self.updateData()
+        #otherwise we need to find where it belongs in this list, so using binary search we find that specific index
         else:
-            self._numbers.insert(findIndex(num, self._numbers))
+            index = findIndex(num, self._numbers)
+            self._numbers.insert(index, num)
 
     def getNumbers(self):
         return self._numbers
+    def updateData(self):
+        self._data[0] = self._numbers[0]
+        self._data[1] = self._numbers[-1]
 
 def initalValues(arr):
 
@@ -55,7 +78,8 @@ def chunkSort(arr):
     
     for index in inital_chunk:
         arr.pop(index)
-        
+
+    #we sort it by inserting each number into the chunk object
     for value in arr:
         chunk.insert(value)
 
@@ -63,16 +87,16 @@ def chunkSort(arr):
                       
 def main():
     
-    test = []
-    second = []
-    size = 100000
-    for i in range(size):
-        test.append(size - i)
-        second.append(size - i)
 
+    #very basic test
+    size = 50000
+    #converting them back into lists so I dont have to rewrite a bunch of the code
+    #using an np.array() would be alot more efficient than python list
+    test = list(np.random.random(size))
+    test_two = test.copy()
     #timing pythons .sort method { timsort}
     sort_start = timer()
-    second.sort()
+    test_two.sort()
     sort_end = timer()
     
     #timing chunksort
@@ -86,6 +110,5 @@ def main():
     print("Chunksort took:", chunk_end - chunk_start)
     print("the list.sort method took:", sort_end - sort_start)
     print(f"Chunksort was {diff} times slower for a list of size {size}")
-    
 if __name__ == "__main__":
     main()
